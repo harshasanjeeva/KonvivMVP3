8d633202fcb826be8336f3ff721dabc06d3ce3a9
harshasanjeeva <harshasanjiva@gmail.com>
2017-11-18 23:13:12 -0800

    make it

--- router.js
+++ router.js
@@ -21,21 +21,59 @@ var app = module.exports = express.Router();
 
 
 
+  app.get('/accounts', function (req, res) {
+    // res.status(200).send({"sucesss": true, "result": "hi"});
+    connection.query("SELECT * FROM accountsTableTest", function (err, result, fields) {
+        if (err) throw err;
+        res.status(200).send({"success": true, "result": result});
+    });
+});
+app.get('/accounts/:id', function (req, res) {
+    if (req.params.id != null) {
+        // connection.query("SELECT * FROM transactionTableTest WHERE account_id = 'rx3maAP9KWf55zQmKyqMTgZ7p4drw4cmq8jy8'", function (err, result) {
+        connection.query("SELECT * FROM transactionTableTest WHERE account_id = ?", [req.params.id], function (err, result) {
+            if (err) throw err;
+            res.status(200).send({"success": true, "result": result});
+        });
+    }
+});
+app.get('/buckets', function(req, res) {
+    connection.query("SELECT * FROM bucketsTest", function(err, result, fields) {
+        if (err) throw err;
+        // console.log(result);
+        res.status(200).send({"success": true, "result": result});
+    });
+});
+app.get('/notifications', function(req, res) {
+    connection.query("SELECT * FROM notificationsTest", function(err, result, fields) {
+        if (err) throw err;
+        res.status(200).send({"success": true, "result": result});
+    });
+});
+app.post('/notifications', function(req, res) {
+    var name = req.body.name;
+    var date = req.body.date;
+    connection.query("INSERT INTO notificationsTest (name, date) VALUES (?, ?)", [name, date], function(err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        res.status(200).send({"success": true, "result": result});
+    });
+});
 app.get('/list', function(req, res){
     connection.connect(function(err) {
         if (err) throw err;
         console.log("Connected!");
-
-      connection.query("INSERT INTO UserTestTable (id, username, password) VALUES ('45', 'Harsha','123456789')", function (err, result, fields) {
-        if (err) throw err;
-         console.log("query successful");
-         console.log(result);
-         res.status(200).send({"sucesss":true, "result":result});
-     });
+     //  connection.query("INSERT INTO UserTestTable (id, username, password) VALUES ('45', 'Harsha','123456789')", function (err, result, fields) {
+     //    if (err) throw err;
+     //     console.log("query successful");
+     //     console.log(result);
+     //     res.status(200).send({"sucesss":true, "result":result});
+     // });
      connection.query("SELECT * FROM UserTestTable", (err, res, fields) => {
        console.log('result is ', res);
+       res.status(200).send({"sucesss":true, "result":result1});
      })
      });
      console.log("this is the JSON data that we are sending");
- //   res.status(200).send({"sucesss":true, "result":result1});
+  
 });
