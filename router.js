7fa7176c1b3d6fa987be15b9ae8dbb9281a76a30
Huimee Sanchez <sanchezh95@gmail.com>
2017-11-22 17:08:50 -0800

    updated router.js

--- router.js
+++ router.js
@@ -4,6 +4,9 @@ var bodyParser = require('body-parser');
 var moment = require('moment');
 var plaid = require('plaid');
 var mysql = require('mysql');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const tokenKey = require('./config/keys');
 
 
 var app = module.exports = express.Router();
@@ -24,6 +27,79 @@ var app = module.exports = express.Router();
         res.status(200).send({"success": true});
 });
 
+app.post('/register', function(req, res){
+    let email = req.body.email;
+    connection.connect(function(err) {
+        connection.query("SELECT * FROM UserTestTable WHERE username = ? ", [email], (err, result, fields) => {
+
+            if(result)  {
+                if(!result[0]) {
+                    bcrypt.genSalt(10, function(err, salt) {
+                        bcrypt.hash(req.body.password, salt, function(err, hash) {
+                            var newUser = {
+                                email: req.body.email,
+                                password: hash
+                            }
+
+                            var userData = [newUser];
+                            var values = [];
+
+                            for (var i = 0; i < 1; i++) {
+                                console.log(userData)
+                                values.push([, userData[i].email, userData[i].password])
+                            }
+
+                            if (err) throw err;
+                            connection.query("INSERT INTO UserTestTable (id, username, password) VALUES ?", [values], function (err, result, fields) {
+                                if (err) throw err;
+                                console.log(result);
+                                res.status(200).send({"success":true, "result":result});
+                            });
+                            connection.query("SELECT * FROM UserTestTable", (err, res, fields) => {
+                                console.log('result is ', res);
+                        })
+
+                        });
+                    });
+                }
+                else if(result[0].username === email) {
+                    console.log(">>>>>>>>>>>>>>>. we are getting the error")
+                    res.send({success: false})
+                }
+            }
+        })
+    });
+});
+
+app.post('/login', function(req, res, next) {
+    connection.connect(function(err) {
+        var email = req.body.email;
+        var password = req.body.password;
+
+        connection.query("SELECT * FROM UserTestTable WHERE username = ? ", [email], function(error, results, fields) {
+            console.log('>>>>>>>>>>>>>> ', results[0].password)
+            let username = results;
+            if( !username || username[0].username !== email || username === undefined || username === null ) {
+                res.send({"sucesss": false, "login": 'no'});
+            }
+            else if(results[0].username === email) {
+                console.log('>>>>>>>>>>> inside else if ', username)
+                let pass = results[0].password
+                if (pass !== undefined && pass !== null ) {
+                    bcrypt.compare(req.body.password, results[0].password, function(err, result) {
+                        if(result) {
+                            return res.send({"sucesss":true, "login":'yes'});
+                        }
+                        else {
+                            res.send({"sucesss": false, "login": 'no'});
+                        }
+                    })
+                }
+            }
+        });
+    });
+});
+
   app.get('/accounts', function (req, res) {
     // res.status(200).send({"sucesss": true, "result": "hi"});
     connection.query("SELECT * FROM accountsTableTest", function (err, result, fields) {
@@ -39,6 +115,12 @@ app.get('/accounts/:id', function (req, res) {
             res.status(200).send({"success": true, "result": result});
         });
     }
+});
+app.get('/transactions', function(req, res){
+    connection.query("SELECT * FROM transactionTable", (err, result, fields) => {
+        console.log('result is ', result);
+        res.status(200).send({"sucesss":true, "result":result});
+    });
 });
 app.get('/buckets', function(req, res) {
     connection.query("SELECT * FROM bucketsTest", function(err, result, fields) {
