d491b0fdec0178d49d4605836109bfbff85f4e69
harshasanjeeva <harshasanjiva@gmail.com>
2017-11-30 23:06:01 -0800

    updated

--- routes/router.js
+++ routes/router.js
@@ -29,89 +29,89 @@ var app = module.exports = express.Router();
 
 var user_id;
 
-app.post('/register', function(req, res){
-    let email = req.body.email;
+// app.post('/register', function(req, res){
+//     let email = req.body.email;
 
-    connection.connect(function(err) {
-        connection.query("SELECT * FROM UserTable WHERE username = ? ", [email], (err, result, fields) => {
+//     connection.connect(function(err) {
+//         connection.query("SELECT * FROM UserTable WHERE username = ? ", [email], (err, result, fields) => {
 
-            if(result)  {
-                if(!result[0]) {
-                    bcrypt.genSalt(10, function(err, salt) {
-                        bcrypt.hash(req.body.password, salt, function(err, hash) {
-                            var newUser = {
-                                email: req.body.email,
-                                password: hash
-                            }
+//             if(result)  {
+//                 if(!result[0]) {
+//                     bcrypt.genSalt(10, function(err, salt) {
+//                         bcrypt.hash(req.body.password, salt, function(err, hash) {
+//                             var newUser = {
+//                                 email: req.body.email,
+//                                 password: hash
+//                             }
 
-                            var userData = [newUser];
-                            var values = [];
-                            user_id = Math.floor(Math.random() * 10000);
+//                             var userData = [newUser];
+//                             var values = [];
+//                             user_id = Math.floor(Math.random() * 10000);
 
-                            for (var i = 0; i < 1; i++) {
-                                console.log(userData)
-                                values.push([user_id, userData[i].email, userData[i].password])
-                            }
+//                             for (var i = 0; i < 1; i++) {
+//                                 console.log(userData)
+//                                 values.push([user_id, userData[i].email, userData[i].password])
+//                             }
 
-                            if (err) throw err;
-                            connection.query("INSERT INTO UserTable (id, username, password) VALUES ?", [values], function (err, result, fields) {
-                                if (err) throw err;
-                                console.log(result);
-                                res.status(200).send({"success":true, "result":result, "user_id":user_id});
-                            });
-                            connection.query("SELECT * FROM UserTable", (err, res, fields) => {
-                                console.log('result is ', res);
-                        })
+//                             if (err) throw err;
+//                             connection.query("INSERT INTO UserTable (id, username, password) VALUES ?", [values], function (err, result, fields) {
+//                                 if (err) throw err;
+//                                 console.log(result);
+//                                 res.status(200).send({"success":true, "result":result, "user_id":user_id});
+//                             });
+//                             connection.query("SELECT * FROM UserTable", (err, res, fields) => {
+//                                 console.log('result is ', res);
+//                         })
 
-                        });
-                    });
-                }
-                else if(result[0].username === email) {
-                    console.log(">>>>>>>>>>>>>>>. we are getting the error")
-                    res.send({success: false})
-                }
-            }
-        })
-    });
-});
+//                         });
+//                     });
+//                 }
+//                 else if(result[0].username === email) {
+//                     console.log(">>>>>>>>>>>>>>>. we are getting the error")
+//                     res.send({success: false})
+//                 }
+//             }
+//         })
+//     });
+// });
 
-app.post('/login', function(req, res) {
-    connection.connect(function(err) {
-        let email = req.body.email;
-        let password = req.body.password;
-        user_id = req.body.user_id;
+// app.post('/login', function(req, res) {
+//     connection.connect(function(err) {
+//         let email = req.body.email;
+//         let password = req.body.password;
+//         user_id = req.body.user_id;
 
-        connection.query("SELECT * FROM UserTable WHERE username = ? ", [email], function(error, results, fields) {
-            if (results[0].password) {
-                bcrypt.compare(req.body.password, results[0].password, function(err, result) {
-                    const token = jwt.sign({email}, tokenKey.jwtSecret);
-                    if(result) {
+//         connection.query("SELECT * FROM UserTable WHERE username = ? ", [email], function(error, results, fields) {
+//             if (results[0].password) {
+//                 bcrypt.compare(req.body.password, results[0].password, function(err, result) {
+//                     const token = jwt.sign({email}, tokenKey.jwtSecret);
+//                     if(result) {
 
-                        var options = {
-                            args: ["hi",user_id]
-                        };
+//                         var options = {
+//                             args: ["hi",user_id]
+//                         };
 
-                        PythonShell.run('bucket-new.py', options, function (err, results) {
-                            //if (err) throw err;
-                            // results is an array consisting of messages collected during execution
-                            console.log('results: %j', results);
-                        });
+//                         PythonShell.run('bucket-new.py', options, function (err, results) {
+//                             //if (err) throw err;
+//                             // results is an array consisting of messages collected during execution
+//                             console.log('results: %j', results);
+//                         });
 
-                        return res.send(
-                            {
-                                "sucesss":true, "login":'yes',
-                                token: token
-                            });
-                    }
-                    else {
-                        console.log('catch all happens?')
-                        return res.status(400).send();
-                    }
-                })
-            }
-        });
-    });
-});
+//                         return res.send(
+//                             {
+//                                 "sucesss":true, "login":'yes',
+//                                 token: token
+//                             });
+//                     }
+//                     else {
+//                         console.log('catch all happens?')
+//                         return res.status(400).send();
+//                     }
+//                 })
+//             }
+//         });
+//     });
+// });
 
   app.get('/accounts', function (req, res) {
     // res.status(200).send({"sucesss": true, "result": "hi"});
